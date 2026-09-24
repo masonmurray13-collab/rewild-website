e85aef9216ad52f8575c63dd9d223a955cac263a
Mason <masonmurray13@gmail.com>
2026-09-24 14:47:52 -0500

    Back to top arrow after first photo

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+// always load the page at the top
+if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+}
+
+window.scrollTo(0, 0);
+
+// make arrow to scroll to top visible only when near the bottom of the page
+const backToTop = document.querySelector(".back-to-top");
+const arrowTrigger = document.querySelector(".arrow-trigger");
+
+if (backToTop && arrowTrigger) {
+    window.addEventListener("scroll", function() {
+        const photoBottom = arrowTrigger.getBoundingClientRect().bottom;
+
+        if (photoBottom <= window.innerHeight) {
+            backToTop.style.display = "block";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+}
+
+// make referral question visible only if "no" is selected for attended
 const attended = document.getElementById("attended");
 const referralQuestion = document.getElementById("referral-question");
 const referral = document.getElementById("referral");
 
-attended.addEventListener("change", function() {
-    if (attended.value === "no") {
-        referralQuestion.style.display = "block";
-        referral.required = true;
-    } else {
-        referralQuestion.style.display = "none";
-        referral.required = false;
-    }
-});
+if (attended) {
+    attended.addEventListener("change", function() {
+        if (attended.value === "no") {
+            referralQuestion.style.display = "block";
+            referral.required = true;
+        } else {
+            referralQuestion.style.display = "none";
+            referral.required = false;
+        }
+    });
+}
